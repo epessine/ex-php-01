505ce54612857eaff05d64ed0931c52378b10e2f
eduar <eduardopessine@gmail.com>
2020-12-29 22:12:33 -0300

    Adiciona validação dos campos

--- script.php
+++ script.php
@@ -15,27 +15,47 @@
         $nome = $_POST['nome'];
         $idade = $_POST['idade'];
 
-        if ($idade < 6) 
+        if (empty($nome) || empty($idade))
         {
-            echo "O candidato(a) $nome não tem idade suficiente.";
+            echo "Todos os campos devem ser preenchidos.";
         }
-        else if ($idade >= 6 && $idade <= 12)
+        else if (strlen($nome) < 3)
         {
-            echo "O candidato(a) $nome pertence à categoria $categorias[0].";
+            echo "O nome deve conter ao menos 3 caracteres.";
         }
-        else if ($idade >= 13 && $idade <= 17)
+        else if (strlen($nome) > 50)
         {
-            echo "O candidato(a) $nome pertence à categoria $categorias[1].";
+            echo "O nome não pode conter mais que 50 caracteres.";
         }
-        else if ($idade >= 18 && $idade <= 59)
+        else if (!is_numeric($idade) || $idade < 0 || $idade > 130)
         {
-            echo "O candidato(a) $nome pertence à categoria $categorias[2].";
+            echo "A idade inserida não é válida.";
         }
-        else if ($idade >= 60)
+        else
         {
-            echo "O candidato(a) $nome pertence à categoria $categorias[3].";
+            if ($idade < 6) 
+            {
+                echo "O(a) candidato(a) $nome não tem idade suficiente.";
+            }
+            else if ($idade >= 6 && $idade <= 12)
+            {
+                echo "O(a) candidato(a) $nome pertence à categoria $categorias[0].";
+            }
+            else if ($idade >= 13 && $idade <= 17)
+            {
+                echo "O(a) candidato(a) $nome pertence à categoria $categorias[1].";
+            }
+            else if ($idade >= 18 && $idade <= 59)
+            {
+                echo "O(a) candidato(a) $nome pertence à categoria $categorias[2].";
+            }
+            else if ($idade >= 60)
+            {
+                echo "O(a) candidato(a) $nome pertence à categoria $categorias[3].";
+            }
         }
+ 
     ?>
-    
+
 </body>
 </html>
